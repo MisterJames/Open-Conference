3001e2f77272b96e67d62020ffe954475d51c8d1
James Chambers <james@jameschambers.com>
2011-11-30 20:49:27 -0600

    Built interface for cache manager and started the switch out. Added Ninject for swapping cache managers when the rebuild is complete.

---
+++ Prdc.Conference/Model/ConferenceDataManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Prdc.Conference.Model
+{
+    public class ConferenceCacheManager : ICacheManager
+    {
+
+        public void UpdateCache() {  }
+
+
+        public bool HasCacheData
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public DateTime? LastUpdate
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler OnNetworkIckiness;
+
+        public event EventHandler<CacheUpdateEventArgs> OnRefreshCompleted;
+
+        public event EventHandler<ProgressEventArgs> SessionProgressReported;
+
+        public event EventHandler<ProgressEventArgs> SpeakersProgressReported;
+    }
+}
